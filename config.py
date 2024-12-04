12b637fa9fc7111ec32f70d462a9936fc581cf2d
yourdilip <108778505+yourdilip@users.noreply.github.com>
2024-12-04 20:05:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 BOT_TOKEN = "YOUR_BOT_TOKEN_HERE"
 
 # Admin user ID (set the ID of the admin)
-ADMIN_USER_ID = 123456789
+ADMIN_USER_ID = 1366227045
 
 # Premium user limits (in terms of video downloads and duration)
 PREMIUM_LIMIT = {
